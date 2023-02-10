70b0239bb68f1b560f5f0a7e10561531174f603f
candy67-123 <zizhao4@s.sfusd.edu>
2023-02-10 15:43:12 -0800

    spawn obstacles at intervals

--- Prototype-3/Assets/Scripts/SpawnManager.cs
+++ Prototype-3/Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject obstaclePerafb;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePerafb, spawnPos, obstaclePerafb.transform.rotation);
     }
 }
