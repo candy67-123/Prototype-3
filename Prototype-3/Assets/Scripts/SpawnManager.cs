bb3b4ce3da343e35d40c097ee77935472e10d91c
candy67-123 <zizhao4@s.sfusd.edu>
2023-02-14 14:13:54 -0800

    destroy the obstacles that exit bounds

--- Prototype-3/Assets/Scripts/SpawnManager.cs
+++ Prototype-3/Assets/Scripts/SpawnManager.cs
@@ -8,10 +8,12 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
+    private PlayerController PlayControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
+        PlayControllerScript = GameObject.Find("player").GetComponent<PlayerController>();
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
@@ -23,6 +25,7 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnObstacle()
     {
+        if(PlayControllerScript.gameOver == false)
         Instantiate(obstaclePerafb, spawnPos, obstaclePerafb.transform.rotation);
     }
 }
